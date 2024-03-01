00d22d3954358099081cdf104dc980b0c84ba56a
Joni Sinokki <jopesino@student.jyu.fi>
2024-03-01 14:32:11 +0200

    list of all courses or all tasks for selected course

--- main.py
+++ main.py
@@ -19,5 +19,24 @@ def logout():
     click.echo("Logout successful.")
 
 
+@tim_ide.command()
+@click.argument("course", required=False)
+def list(course=None):
+    """
+    Lists user courses. If course is provided, it will list the course tasks.
+
+    Usage:
+    tim_ide list [OPTIONS] [COURSE]
+
+    Options:
+    COURSE  Course name (not required)
+    """
+    # Do something
+    if course:
+        click.echo(f"Listed tasks for course {course}")
+    else:
+        click.echo("Listed all courses")
+
+
 if __name__ == "__main__":
     tim_ide()
