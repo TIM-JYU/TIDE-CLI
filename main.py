57f22bc1b96fcadbddad6c379150ae4a2a2d5cf7
Joni Sinokki <jopesino@student.jyu.fi>
2024-03-01 14:46:55 +0200

    pull/push course data or task

--- main.py
+++ main.py
@@ -38,5 +38,45 @@ def list(course=None):
         click.echo("Listed all courses")
 
 
+@tim_ide.command()
+@click.argument("course")
+@click.option("--task",  help="Specific task to pull")
+def pull(course, task=None):
+    """
+    Fetches course or task data
+    
+    Usage:
+    tim_ide pull [OPTIONS] COURSE
+    
+    Options:
+    --task NAME (not required)
+    """
+    # Do something
+    if task:
+        click.echo(f"Pulled task {task} for course {course}")
+    else:
+        click.echo(f"Pulled course {course}")
+
+
+@tim_ide.command()
+@click.argument("course")
+@click.option("--task",  help="Specific task to submit")
+def push(course, task=None):
+    """
+    Submits course or task data
+    
+    Usage:
+    tim_ide push [OPTIONS] COURSE
+    
+    Options:
+    --task NAME (not required)
+    """
+    # Do something
+    if task:
+        click.echo(f"Pushed task {task} for course {course}")
+    else:
+        click.echo(f"Pushed course {course}")
+
+
 if __name__ == "__main__":
     tim_ide()
