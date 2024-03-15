887f02f5247e37a26807d85a4ba10307c9e4fa02
O-P <ylivuotokanava@gmail.com>
2024-03-15 13:00:45 +0200

    Unit tests for filesaver (#7)
    * Unit tests for filesaver
    
    * Module changed, test update for filecreation
    
    * Error logger util add
    
    * Handling for request, message style add
    
    * Test for file creation
    
    * Minor fix test, py ver downgrade
    
    * Testdata fix

---
+++ tests/test_utils.py
@@ -0,0 +1,29 @@
+import os
+import unittest
+from src.tidecli.utils import file_saver
+# Testdata for creating filestructure
+file_data = {
+    "code": "print('Hello World1')",
+    "path": "main.py",
+    "ide_task_id": "T1",
+    "paragraph_id": "123",
+}
+
+
+class TestCreateFolders(unittest.TestCase):
+    """
+    Test the folder structure creation
+    """
+
+    def test_create_folders():
+        pass
+
+
+class TestCreateFile(unittest.TestCase):
+    def test_create_task_file():
+        """
+        Test helper function for creating a file
+        """
+        
+        file = file_saver.create_task_file(file_data)
+        assert os.path.exists(os.path.join(file, 'main.py'))
