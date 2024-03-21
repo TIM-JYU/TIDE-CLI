1fdd3a235a20321d17319394cd172bcd8b25f3f5
olrut <121128961+olrut@users.noreply.github.com>
2024-03-21 08:17:20 +0200

    Delete src/tidecli/utils/helpers.py
    Not needed

--- src/tidecli/utils/helpers.py
+++
@@ -1,7 +0,0 @@
-def json_to_key_value_pairs(json_data: dict):
-    """
-    Convert json to key value pairs
-    """
-    return {
-        course["course_name"]: course["course_id"] for course in json_data["courses"]
-    }
