4ce9de603bde3dcc58afc446e651226a53d3453d
olrut <121128961+olrut@users.noreply.github.com>
2024-03-26 19:39:09 +0200

    Update SubmitData.py
    Added support for giving input and args when submitting

--- src/tidecli/models/SubmitData.py
+++ src/tidecli/models/SubmitData.py
@@ -11,6 +11,8 @@ class SubmitData(BaseModel):
     task_id: str
     doc_id: int
     code_language: str  # Plugin "type"
+    user_input: str = ""
+    user_args: str = ""
 
     def submit_json(self):
         if isinstance(self.code_files, TaskFile):
@@ -18,5 +20,7 @@ class SubmitData(BaseModel):
         return {
             "code_files": [file.to_json() for file in self.code_files],
             "code_language": self.code_language,
-            "task_id_ext": str(self.doc_id) + "." + self.task_id  # "<doc_id>.<task_id>"
+            "task_id_ext": str(self.doc_id) + "." + self.task_id,  # "<doc_id>.<task_id>"
+            "user_input": self.user_input,
+            "user_args": self.user_args,
         }
