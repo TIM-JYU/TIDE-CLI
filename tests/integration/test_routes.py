0377e62a6ce7a14e1411fcd869c1fae11f0e037c
ylivuoto <ylivuotokanava@gmail.com>
2024-11-01 12:13:35 +0200

    Empty function for test submit

--- tests/integration/test_routes.py
+++ tests/integration/test_routes.py
@@ -6,7 +6,6 @@ import tim_api
 
 USERNAME = os.environ.get("TIM_USERNAME", "testuser1")
 
-
 def test_get_profile(user_setup):
     """Get user profile, check username correctness."""
     res = tim_api.get_current_user()
@@ -23,4 +22,9 @@ def test_get_courses():
     # TODO: Keyring might be possible to bypass on testing
 
     # assert isinstance(res, list)
+    pass
+
+def test_submit_answer():
+    """Submit an answer to a course."""
+    # TODO: Write small codefile and submit it
     pass
