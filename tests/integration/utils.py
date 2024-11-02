ae02dff6e4243fcde1012a8ab0322055567e28bc
hannesi <hannesi@iki.fi>
2024-11-02 11:55:11 +0200

    quick notes

--- tests/integration/utils.py
+++ tests/integration/utils.py
@@ -4,7 +4,6 @@ def auth_test_user():
     session = get_session()
     login("testuser1", "test1pass", session)
     
-
 def setup_tim_test_data():
     # TODO: Luo testidataa/dokumentteja ide clitä varten
     pass
@@ -12,3 +11,14 @@ def setup_tim_test_data():
 def teardown_tim_test_data():
     # TODO: Poista testitapaus dokumentit lokaalista timistä
     pass
+
+# HOX: testauskurssilla törmäsin tällaseen muotoon yllä olevien kahden funkkarin sijaan
+@pytest.fixture(scope="session", autouse=True)
+def tim_test_data():
+    # setup
+
+    # wait for tests to run
+    yield
+    
+    # teardown
+
