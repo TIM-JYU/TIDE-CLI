202ddce15a776bad5b99db8a37787c38a4e4bb8e
hannesi <hannesi@iki.fi>
2024-11-28 17:11:49 +0200

    Happy path test for submit

--- tests/integration/test_command_task_submit.py
+++ tests/integration/test_command_task_submit.py
@@ -3,20 +3,53 @@ from pathlib import Path
 import pytest
 from click.testing import CliRunner
 
-from tidecli.main import task
+from tidecli.main import submit
+from constants import TEMPORARY_DIRECTORY
+from utils import copy_directory_from_expected_to_temporary
 
-def test_task_submit():
+
+def test_task_submit(tmp_dir):
     """Submit an answer to a task."""
-    pass
+    answer_str = "#the horse drinks heineken" 
+
+    exercise_id = "exercise-b"
+    task_id = "t1"
+    copy_directory_from_expected_to_temporary(exercise_id)
+
+    # edit the file to be submitted
+    with open(Path(TEMPORARY_DIRECTORY, exercise_id, task_id, "hevonen.py"), "r+") as f:
+        file_content = f.readlines()
+        file_content[0] = answer_str
+
+        f.seek(0)
+        f.write("\n".join(file_content))
+        f.truncate()
+
+    runner = CliRunner()
+    res = runner.invoke(
+        submit,
+        [
+            str(Path(TEMPORARY_DIRECTORY, exercise_id, task_id)),
+        ],
+    )
+
+    assert res.exit_code == 0
+    assert "Saved new answer successfully." in res.output
 
 
+@pytest.mark.xfail
 def test_task_submit_invalid_path():
     pass
+    pytest.xfail()
 
 
+@pytest.mark.xfail
 def test_task_submit_invalid_answer_file():
     pass
+    pytest.xfail()
 
 
+@pytest.mark.xfail
 def test_task_submit_invalid_meta_data():
     pass
+    pytest.xfail()
