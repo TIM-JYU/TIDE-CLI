ac0cd822e89e458d39dd9aa592540a151e416a0c
hannesi <hannesi@iki.fi>
2024-12-05 12:54:13 +0200

    login tests

--- tests/integration/test_command_login.py
+++ tests/integration/test_command_login.py
@@ -1,27 +1,54 @@
-def test_login():
+from os import name
+from click.testing import CliRunner
+from playwright.sync_api import Playwright
+import pytest
+import webbrowser
+
+from conftest import user1
+from tidecli.main import login, logout
+
+def test_login(playwright: Playwright, monkeypatch: pytest.MonkeyPatch):
+    def handle_login(url: str):
+        browser = playwright.chromium.launch(headless=False)
+        ctx = browser.new_context()
+        page = ctx.new_page()
+
+        # Navigate to the auth page
+        page.goto(url)
+
+        # Click login button
+        page.get_by_role("button", name="Log in").click()
+
+        # Click "Email login"
+        page.get_by_role("button", name="Email login").click()
+
+        # Fill in login info
+        page.fill("#email", user1.username)
+        page.fill("#password", user1.password)
+
+        # Click "Log in"
+        page.locator("tim-login-dialog").get_by_role("button", name="Log in").click()
+
+        # Click the authentication button
+        page.click("input[value='Authenticate to TIDE']")
+        
+        # Wait for auth success
+        page.locator("text='Login successful! You can now close this tab.'").wait_for()
+
+        page.close()
+
+    monkeypatch.setattr(webbrowser, "open", handle_login)
+
+    runner = CliRunner()
+
+    res = runner.invoke(login)
+    print(res)
+
     pass
 
 
 def test_login_json():
+    runner = CliRunner()
+    res = runner.invoke(logout)
     pass
 
-
-# def mock_browser_open(url: str):
-# logger = logging.getLogger('selenium')
-# logger.setLevel(logging.DEBUG)
-# logger.debug("hello from selenium")
-# driver = webdriver.Firefox()
-# driver.get(url)
-# driver.implicitly_wait(2)
-# login_form = driver.find_element(By.CLASS_NAME, "logolink")
-# login_form.click()
-# print(f"url: {url}")
-# return
-
-
-# def test_login(monkeypatch: pytest.MonkeyPatch):
-# monkeypatch.setattr("webbrowser.open", mock_browser_open)
-# import tidecli.main as app
-# runner = CliRunner()
-# result = runner.invoke(app.login)
-# assert result == -1
