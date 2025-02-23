e8c288a2e0051a77afe4fc96af17a68c9e763083
pekMant <116541374+pekMant@users.noreply.github.com>
2025-02-23 21:09:19 +0200

    add unit test for create-course

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -12,6 +12,7 @@ from unit.test_data import (
     validate_token_response,
     get_tasks_by_doc_test_response,
     get_task_by_ide_task_id_test_response,
+    get_tasks_by_course_test_response,
 )
 from unit.test_routes import _create_mock_request
 from tidecli.main import login, logout, courses, task
@@ -87,7 +88,7 @@ class TestMain(unittest.TestCase):
 
     @patch("tidecli.api.routes.requests.request")
     @patch("tidecli.api.routes.get_signed_in_user")
-    @patch("tidecli.main.is_logged_in") 
+    @patch("tidecli.main.is_logged_in")
     def test_courses(self, mock_is_logged_in, mock_get_signed_in_user, mock_request):
         """
         Test listing courses
@@ -135,7 +136,9 @@ class TestMainFileAccess(TestCase):
     @patch("tidecli.api.routes.requests.request")
     @patch("tidecli.api.routes.get_signed_in_user")
     @patch("tidecli.main.is_logged_in")
-    def test_task_create_all(self, mock_is_logged_in, mock_get_signed_in_user, mock_request):
+    def test_task_create_all(
+        self, mock_is_logged_in, mock_get_signed_in_user, mock_request
+    ):
         """
         Test creating all tasks and trying to overwrite them without the -f flag
         """
@@ -152,12 +155,16 @@ class TestMainFileAccess(TestCase):
             ],
         )
 
-        file_path_1 = Path(self.working_dir, "Demo1", "t1").relative_to(self.working_dir)
-        file_path_2 = Path(self.working_dir, "Demo1", "t2").relative_to(self.working_dir)
+        file_path_1 = Path(self.working_dir, "Demo1", "t1").relative_to(
+            self.working_dir
+        )
+        file_path_2 = Path(self.working_dir, "Demo1", "t2").relative_to(
+            self.working_dir
+        )
 
         self.assertEqual(
             result.output,
-            f"Wrote file {file_path_1}: test.c\nWrote file {file_path_2}: test.c\n"
+            f"Wrote file {file_path_1}: test.c\nWrote file {file_path_2}: test.c\n",
         )
 
         test_path1 = f"{self.working_dir}Demo1/t1"
@@ -171,7 +178,6 @@ class TestMainFileAccess(TestCase):
         self.assertTrue(os.path.exists(test_path2))
         self.assertTrue(os.path.exists(test_file2))
         self.assertTrue(os.path.exists(test_metadata))
-
 
         result_overwrite = self.runner.invoke(
             task,
@@ -192,7 +198,9 @@ class TestMainFileAccess(TestCase):
     @patch("tidecli.api.routes.requests.request")
     @patch("tidecli.api.routes.get_signed_in_user")
     @patch("tidecli.main.is_logged_in")
-    def test_task_create_one(self, mock_is_logged_in, mock_get_signed_in_user, mock_request):
+    def test_task_create_one(
+        self, mock_is_logged_in, mock_get_signed_in_user, mock_request
+    ):
         """
         Test creating a single task
         """
@@ -212,9 +220,7 @@ class TestMainFileAccess(TestCase):
         )
 
         file_path = Path(self.working_dir, "Demo1", "t3").relative_to(self.working_dir)
-        self.assertEqual(
-            result.output, f"Wrote file {file_path}: test.c\n"
-        )
+        self.assertEqual(result.output, f"Wrote file {file_path}: test.c\n")
 
         test_path1 = f"{self.working_dir}Demo1/t3"
         test_metadata1 = f"{self.working_dir}.timdata"
@@ -222,3 +228,110 @@ class TestMainFileAccess(TestCase):
         self.assertTrue(os.path.exists(test_path1))
         self.assertTrue(os.path.exists(test_metadata1))
         self.assertTrue(os.path.exists(test_file1))
+
+    @patch("tidecli.api.routes.requests.request")
+    @patch("tidecli.api.routes.get_signed_in_user")
+    @patch("tidecli.main.is_logged_in")
+    def test_create_course_by_path(
+        self, mock_is_logged_in, mock_get_signed_in_user, mock_request
+    ):
+        """
+        Test fetching and creating all tasks from a course path
+        """
+        self.maxDiff = None
+        mock_is_logged_in.return_value = True
+        mock_get_signed_in_user.return_value = User("test", "test")
+        mock_request.return_value = _create_mock_request(
+            get_tasks_by_course_test_response
+        )
+
+        result = self.runner.invoke(
+            task,
+            ["create-course", "--path", "kurssit/tie/ohj2/2025k/kotisivu"],
+        )
+
+        file_path_1 = Path(self.working_dir, "demo1", "t1").relative_to(
+            self.working_dir
+        )
+        file_path_2 = Path(self.working_dir, "demo1", "t2").relative_to(
+            self.working_dir
+        )
+        file_path_3 = Path(self.working_dir, "demo1", "t3").relative_to(
+            self.working_dir
+        )
+        file_path_4 = Path(self.working_dir, "demo2", "33232123").relative_to(
+            self.working_dir
+        )
+        file_path_5 = Path(self.working_dir, "demo3", "t1").relative_to(
+            self.working_dir
+        )
+        file_path_6 = Path(self.working_dir, "demo3", "t2").relative_to(
+            self.working_dir
+        )
+        file_path_7 = Path(self.working_dir, "demo3", "t4").relative_to(
+            self.working_dir
+        )
+
+        self.assertEqual(
+            result.output,
+            f"Wrote file {file_path_1}: hello.cs\n"
+            f"Wrote file {file_path_1}: t1.csproj\n"
+            f"Wrote file {file_path_2}: animals.py\n"
+            f"Wrote file {file_path_2}: kissa.txt\n"
+            f"Wrote file {file_path_2}: koira.dat\n"
+            f"Wrote file {file_path_3}: hello.cs\n"
+            f"Wrote file {file_path_3}: t3.csproj\n"
+            f"Wrote file {file_path_4}: hello.cs\n"
+            f"Wrote file {file_path_4}: 33232123.csproj\n"
+            f"Wrote file {file_path_5}: hello.cs\n"
+            f"Wrote file {file_path_5}: t1.csproj\n"
+            f"Wrote file {file_path_6}: hello.py\n"
+            f"Wrote file {file_path_7}: laskuri.cs\n"
+            f"Wrote file {file_path_7}: t4.csproj\n",
+        )
+
+        test_path1 = f"{self.working_dir}demo1/t1"
+        test_path2 = f"{self.working_dir}demo1/t2"
+        test_path3 = f"{self.working_dir}demo1/t3"
+        test_path4 = f"{self.working_dir}demo2/33232123"
+        test_path5 = f"{self.working_dir}demo3/t1"
+        test_path6 = f"{self.working_dir}demo3/t2"
+        test_path7 = f"{self.working_dir}demo3/t4"
+
+        test_file1 = f"{test_path1}/hello.cs"
+        test_file2 = f"{test_path1}/t1.csproj"
+        test_file3 = f"{test_path2}/animals.py"
+        test_file4 = f"{test_path2}/kissa.txt"
+        test_file5 = f"{test_path2}/koira.dat"
+        test_file6 = f"{test_path3}/hello.cs"
+        test_file6 = f"{test_path3}/t3.csproj"
+        test_file7 = f"{test_path4}/hello.cs"
+        test_file8 = f"{test_path4}/33232123.csproj"
+        test_file9 = f"{test_path5}/hello.cs"
+        test_file10 = f"{test_path5}/t1.csproj"
+        test_file11 = f"{test_path6}/hello.py"
+        test_file12 = f"{test_path7}/laskuri.cs"
+        test_file13 = f"{test_path7}/t4.csproj"
+        test_metadata = f"{self.working_dir}.timdata"
+
+        self.assertTrue(os.path.exists(test_path1))
+        self.assertTrue(os.path.exists(test_path2))
+        self.assertTrue(os.path.exists(test_path3))
+        self.assertTrue(os.path.exists(test_path4))
+        self.assertTrue(os.path.exists(test_path5))
+        self.assertTrue(os.path.exists(test_path6))
+        self.assertTrue(os.path.exists(test_path7))
+        self.assertTrue(os.path.exists(test_file1))
+        self.assertTrue(os.path.exists(test_file2))
+        self.assertTrue(os.path.exists(test_file3))
+        self.assertTrue(os.path.exists(test_file4))
+        self.assertTrue(os.path.exists(test_file5))
+        self.assertTrue(os.path.exists(test_file6))
+        self.assertTrue(os.path.exists(test_file7))
+        self.assertTrue(os.path.exists(test_file8))
+        self.assertTrue(os.path.exists(test_file9))
+        self.assertTrue(os.path.exists(test_file10))
+        self.assertTrue(os.path.exists(test_file11))
+        self.assertTrue(os.path.exists(test_file12))
+        self.assertTrue(os.path.exists(test_file13))
+        self.assertTrue(os.path.exists(test_metadata))
